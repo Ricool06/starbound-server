a7757535e2fc3cafbbb37386f48ce4477d5df347
Ricool06 <ricoolohsix@hotmail.com>
2019-06-28 22:27:55 +0100

    Refactor bootstrapping to use module on Github

--- configs/bootstrap_backend/main.tf
+++ configs/bootstrap_backend/main.tf
@@ -1,16 +1,5 @@
 module "bootstrap_backend" {
-  source = "./../../modules/s3_backend"
+  source = "github.com/Ricool06/bootstrap-s3-backend"
   project_name = "starbound-server"
-}
-
-resource "null_resource" "backend_templater" {
-  provisioner "local-exec" {
-    command =<<EOF
-      cd ../starbound_server && \
-      BUCKET=${module.bootstrap_backend.backend_bucket_name} \
-      DB_TABLE=${module.bootstrap_backend.backend_table_name} \
-      REGION=${module.bootstrap_backend.aws_region} \
-      bash -c 'envsubst < backend.tf.tmpl > backend.tf'
-    EOF
-  }
+  path_to_backend_template_file = "../starbound_server/backend.tf.tmpl"
 }
