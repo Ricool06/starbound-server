a5f8c1ed441c25d17a818fa04ca3eb6b707e37e0
Ricool06 <ricoolohsix@hotmail.com>
2019-06-28 23:11:34 +0100

    Set up empty SSH-enabled server

--- configs/bootstrap_backend/main.tf
+++ configs/bootstrap_backend/main.tf
@@ -1,5 +1,5 @@
 module "bootstrap_backend" {
   source = "github.com/Ricool06/bootstrap-s3-backend"
-  project_name = "starbound-server"
+  project_name = "${var.project_name}"
   path_to_backend_template_file = "../starbound_server/backend.tf.tmpl"
 }
